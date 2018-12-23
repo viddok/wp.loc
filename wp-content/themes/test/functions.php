844c4fa0109b70d4990215c6793745844b54a50b
Evgenii Rodkin <rodkin1987@gmail.com>
2018-12-23 19:44:00 +0200

    Включил sidebar

--- wp-content/themes/test/functions.php
+++ wp-content/themes/test/functions.php
@@ -30,3 +30,22 @@ function delete_width_height($image, $attachment_id, $size, $icon){
 	$image[2] = '';
 	return $image;
 }
+
+/** Регистрация бокового sidebar */
+function true_register_wp_sidebars() {
+
+	/* В боковой колонке */
+	register_sidebar(
+		array(
+			'id' => 'true_side', // уникальный id
+			'name' => 'Боковая колонка', // название сайдбара
+			'description' => 'Перетащите сюда виджеты, чтобы добавить их в сайдбар.', // описание
+			'before_widget' => '<div id="%1$s" class="card my-4 %2$s">', // по умолчанию виджеты выводятся <li>-списком
+			'after_widget' => '</div>',
+			'before_title' => '<h5 class="card-header">', // по умолчанию заголовки виджетов в <h2>
+			'after_title' => '</h3>'
+		)
+	);
+}
+
+add_action( 'widgets_init', 'true_register_wp_sidebars' ); // Хук widgets_init обязателен!
