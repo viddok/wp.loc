ccdc038adfbfe9b2af70e41a7a6464ce01432841
Evgenii Rodkin <rodkin1987@gmail.com>
2018-12-24 06:45:37 +0200

    Добавил блок Автора

--- wp-content/themes/test/single.php
+++ wp-content/themes/test/single.php
@@ -32,6 +32,19 @@
 				</div>
 			<?php endwhile; endif; ?>
 
+            <div class="author-container">
+                <div class="author-data-container">
+                    <span class="author-name">
+                        <?php the_author_posts_link() ?>
+                    </span>
+	                <?php echo get_avatar( 'user_email', 80, 'https://poplanete.ru/templates/New_poplanete/dleimages/noavatar.png' ); ?>
+                </div>
+                <div class="author-info">
+	                <?php the_author_meta( 'description' ); ?>
+                </div>
+                <div class="clr"></div>
+            </div>
+
 			<?php get_comment() ?>
 
 			<!-- Pagination -->
