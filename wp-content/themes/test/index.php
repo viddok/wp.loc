3df6286437308031a5e90b89c678fdb4f3fa2972
Evgenii Rodkin <rodkin1987@gmail.com>
2018-12-25 09:00:24 +0200

    Локализовал index.php

--- wp-content/themes/test/index.php
+++ wp-content/themes/test/index.php
@@ -25,22 +25,30 @@
 
             <!-- Pagination -->
             <ul class="pagination justify-content-center mb-4">
+
+	            <?php $button_name = '&larr; ' . __( 'Older', 'translate') ?>
                 <?php if (!empty(get_previous_posts_link())) : ?>
                     <li class="page-item">
-                        <?php previous_posts_link('&larr; Older') ?>
+                        <?php previous_posts_link($button_name) ?>
                     </li>
                 <?php else: ?>
                     <li class="page-item disabled">
-                        <a class="page-link" href="#">&larr; Older</a>
+                        <a class="page-link" href="#">
+	                        <?php echo $button_name ?>
+                        </a>
                     </li>
                 <?php endif; ?>
+
+                <?php $button_name = __( 'Newer', 'translate') . ' &rarr;'; ?>
 	            <?php if (!empty(get_next_posts_link())) : ?>
                     <li class="page-item">
-			            <?php next_posts_link('Newer &rarr;') ?>
+			            <?php next_posts_link($button_name) ?>
                     </li>
 	            <?php else: ?>
                     <li class="page-item disabled">
-                        <a class="page-link" href="#">Newer &rarr;</a>
+                        <a class="page-link" href="#">
+                            <?php echo $button_name ?>
+                        </a>
                     </li>
 	            <?php endif; ?>
             </ul>
