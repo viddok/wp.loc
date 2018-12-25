4cefb668791e4f6f97a81a691b25c3a58e65859b
Evgenii Rodkin <rodkin1987@gmail.com>
2018-12-25 08:49:59 +0200

    Локализовал footer.php

--- wp-content/themes/test/footer.php
+++ wp-content/themes/test/footer.php
@@ -1,7 +1,12 @@
 <!-- Footer -->
 <footer class="py-5 bg-dark">
 	<div class="container">
-		<p class="m-0 text-center text-white">Copyright &copy; <?php bloginfo() ?> <?php echo date('Y') ?></p>
+		<p class="m-0 text-center text-white">
+            <?php
+            $copyright = __('Copyright') . '&copy; ' . bloginfo() . ' ' . date('Y');
+            echo $copyright;
+            ?>
+        </p>
 	</div>
 	<!-- /.container -->
 </footer>
