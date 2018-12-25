6e7db11bf8b724bd2c55b1f4de4508ce562c22c1
Evgenii Rodkin <rodkin1987@gmail.com>
2018-12-25 08:38:32 +0200

    Локализовал content.php

--- wp-content/themes/test/content.php
+++ wp-content/themes/test/content.php
@@ -1,21 +1,24 @@
 <!-- Blog Post -->
 <div class="card mb-4">
-    <?php if (has_post_thumbnail()) :
-        $attr = array(
-	        'class'	=> 'card-img-top'
-        );
-        the_post_thumbnail('large', $attr);
-    else:
-        echo '<img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">';
-    endif; ?>
+	<?php
+	if ( has_post_thumbnail() ) :
+		$attr = array(
+			'class' => 'card-img-top'
+		);
+		the_post_thumbnail( 'large', $attr );
+	endif;
+	?>
     <div class="card-body">
         <a href="<?php echo get_permalink() ?>" style="color: #1d2124; :hover">
             <h2 class="card-title"><?php the_title() ?></h2>
         </a>
-        <?php the_content() ?>
+		<?php the_content() ?>
     </div>
     <div class="card-footer text-muted">
-        Posted on <?php echo get_the_date('F j, Y') ?> by
-        <?php the_author_posts_link() ?>
+        <?php
+        $posted = __( 'Posted on', 'translate') . ' ' . get_the_date( 'F j, Y' ) . ' ' .
+                  __( 'by', 'translate') . ' ' . get_the_author_posts_link();
+        echo $posted;
+        ?>
     </div>
 </div>
