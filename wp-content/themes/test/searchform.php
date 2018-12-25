6433b1ff49aa7bd37c2cb81d55e3d670edf43809
Evgenii Rodkin <rodkin1987@gmail.com>
2018-12-25 09:06:10 +0200

    Локализовал searchform.php

--- wp-content/themes/test/searchform.php
+++ wp-content/themes/test/searchform.php
@@ -1,12 +1,13 @@
 <div class="card my-4">
-    <h5 class="card-header">Search</h5>
+    <h5 class="card-header">
+        <?php _e( 'Search', 'translate') ?>
+    </h5>
     <div class="card-body">
         <div class="input-group">
             <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ) ?>" >
-                <input type="text" name="s" id="s" class="form-control" placeholder="Search for...">
+                <input type="text" name="s" id="s" class="form-control" placeholder=" <?php _e( 'Search for ...', 'translate') ?>">
                 <span class="input-group-btn">
-                    <input type="submit" id="searchsubmit" class="btn btn-secondary" value="Go!" />
-                   <!-- <button id="searchsubmit" class="btn btn-secondary" type="button">Go!</button>-->
+                    <input type="submit" id="searchsubmit" class="btn btn-secondary" value="<?php _e( 'Go!', 'translate') ?>" />
                 </span>
             </form>
         </div>
