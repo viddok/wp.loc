988333ff490bb9e2178e6d42bc49c2f6eb7cb867
Evgenii Rodkin <rodkin1987@gmail.com>
2018-12-27 08:23:03 +0200

    Пофиксил внешний вид формы поиска

--- wp-content/themes/test/searchform.php
+++ wp-content/themes/test/searchform.php
@@ -1,15 +1,17 @@
 <div class="card my-4">
     <h5 class="card-header">
-        <?php _e( 'Search', 'translate') ?>
+		<?php _e( 'Search', 'translate' ) ?>
     </h5>
-    <div class="card-body">
-        <div class="input-group">
-            <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ) ?>" >
-                <input type="text" name="s" id="s" class="form-control" placeholder=" <?php _e( 'Search for ...', 'translate') ?>">
+    <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ) ?>">
+        <div class="card-body">
+            <div class="input-group">
+                <input type="text" name="s" id="s" class="form-control"
+                       placeholder=" <?php _e( 'Search for ...', 'translate' ) ?>">
                 <span class="input-group-btn">
-                    <input type="submit" id="searchsubmit" class="btn btn-secondary" value="<?php _e( 'Go!', 'translate') ?>" />
+                    <input type="submit" id="searchsubmit" class="btn btn-secondary"
+                           value="<?php _e( 'Go!', 'translate' ) ?>"/>
                 </span>
-            </form>
+            </div>
         </div>
-    </div>
+    </form>
 </div>
