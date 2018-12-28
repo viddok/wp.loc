885c6a0d1d73c9d9edec9ef8cfa5566e3b37fe54
Evgenii Rodkin <rodkin1987@gmail.com>
2018-12-28 07:48:21 +0200

    Создал шаблоны для Page

---
+++ wp-content/themes/test/page.php
@@ -0,0 +1,29 @@
+<?php get_header() ?>
+
+<!-- Page Content -->
+<div class="container">
+
+    <div class="row">
+
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+
+            <h1 class="my-4"><?php the_title() ?></h1>
+
+			<?php if ( have_posts() ): while ( have_posts() ): the_post();
+				get_template_part( 'template-parts/content', 'page' );
+			endwhile; endif; ?>
+
+            <!--Подключение файла комментариев-->
+			<?php comments_template(); ?>
+        </div>
+
+		<?php get_sidebar() ?>
+
+    </div>
+    <!-- /.row -->
+
+</div>
+<!-- /.container -->
+
+<?php get_footer() ?>
---
+++ wp-content/themes/test/template-parts/content-page.php
@@ -0,0 +1,7 @@
+<!-- Blog Post -->
+<div class="card mb-4">
+    <div class="card-body">
+		<?php the_content(); ?>
+    </div>
+</div>
+<!-- /Blog Post -->
